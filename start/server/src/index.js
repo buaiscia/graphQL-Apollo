156b79addfb57c0821cb615741542756fbcfad7b
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-01-15 11:54:36 +0100

    created server

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -2,3 +2,8 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema'); // create the object with the schema
 
 const server = new ApolloServer({ typeDefs }); // pass the schema into the server
+
+server.listen()
+    .then(({ url }) => {
+        console.log(`🚀 Server ready at ${url}`)
+    })
